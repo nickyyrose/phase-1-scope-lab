5aec273540d38b305cf4695fe9cb3c90bafc389e
Nicholas Rose <ntrose90@gmail.com>
2024-03-29 08:14:53 -0700

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+customerName = 'bob'; 
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase(); 
+  return customerName; 
+}
+
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'; 
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'; 
+}
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = 'susan'; 
+  leastFavoriteCustomer = 'john'; 
+}
+
+module.exports = { customerName, upperCaseCustomerName, setBestCustomer, overwriteBestCustomer, changeLeastFavoriteCustomer };
